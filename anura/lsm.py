0c081d20c181c0272200368ad7a02995cef600a5
Daniel Morales <daniel.morales@revolut.com>
2023-04-19 23:02:22 +0200

    ANURA-1 wrapper class for memtable

--- anura/lsm.py
+++ anura/lsm.py
@@ -1,9 +1,26 @@
 from typing import Dict, Optional
 
 
-class LSMTree:
+class MemTable:
     def __init__(self) -> None:
         self.__mem_table: Dict[str, str] = {}
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.__mem_table
+
+    def __getitem__(self, key: str) -> str:
+        return self.__mem_table[key]
+
+    def __setitem__(self, key: str, value: str) -> None:
+        self.__mem_table[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.__mem_table[key]
+
+
+class LSMTree:
+    def __init__(self) -> None:
+        self.__mem_table = MemTable()
 
     def get(self, key: str) -> Optional[str]:
         if key in self.__mem_table:
